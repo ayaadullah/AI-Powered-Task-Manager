7179c4b5a588d0e8d8e3c965ebc0666128041c27
Ayaad Ullah Bayg <ayadullahbaig69@gmail.com>
2026-09-02 09:14:43 +0500

    feat: add Code Snippet Organizer application entry point

---
+++ code-snippet-organizer/backend/src/main/java/com/ayaadullah/codesnippetorganizer/CodeSnippetOrganizerApplication.java
@@ -0,0 +1,12 @@
+package com.ayaadullah.codesnippetorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSnippetOrganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeSnippetOrganizerApplication.class, args);
+    }
+}
